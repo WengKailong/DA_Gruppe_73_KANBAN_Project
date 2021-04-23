83adf2608fa601670a578a78fdd9ccf50a022bd7
kailong <wengkailong@gmail.com>
2021-04-23 16:10:56 +0200

    first structure added

---
+++ main.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+let allTasks = [];
+
+let task;
+
+function addTask(name, category) {
+  let taskName = document.getElementById("task-name").value;
+  let category = document.getElementById("category").value;
+
+  console.log(taskName);
+  console.log(category);
+
+  let task = {
+    name: taskName,
+    category: category,
+    createdAt: new Date().toISOString(),
+  };
+
+  allTasks.push(task);
+
+  let allTasksAsString = JSON.stringify(allTasks);
+
+  localStorage.setItem('savedTasks', allTasksAsString);
+  console.log(allTasks);
+
+
+}
+
+function loadAllTasks(){
+    let allTasksAsString = localStorage.getItem('savedTasks');
+    allTasks = JSON.parse(allTasksAsString);
+
+    console.log(allTasks);
+}
