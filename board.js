021ef3a1b6490feb1a23491cf166281aec4fe075
TimmEichholz <karate3ule@hotmail.de>
2021-04-24 22:16:50 +0200

    board: finish starting structure, did general css for Board

---
+++ board.js
@@ -0,0 +1,16 @@
+allTasks =[
+    {
+        id : 1,
+        title : "test",
+        category : "Management",
+        description : "lorem Ipsum dolor sit amet, consectetur",
+        status: "todo",
+        creator: "Timm Eichholz",
+        creationdate: ""
+    }
+];
+
+function writeTasks(id){
+
+
+}
