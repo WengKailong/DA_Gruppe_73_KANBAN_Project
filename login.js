c5c745c805c5a6d738318935a811515459437916
kailong <wengkailong@gmail.com>
2021-06-04 19:55:15 +0200

    bugs fixed

--- login.js
+++ login.js
@@ -1,76 +1,66 @@
 let loginUser = {
   userId: -1,
-  userName: '',
-  userPassword: ''
-}
-var formData
+  userName: "",
+  userPassword: "",
+};
+var formData;
 
-let selectedAvatar = '/img/profile.png';
+let selectedAvatar = "/img/profile.png";
 
-function guestLogin(){
-loginUser.userName = 'guest';
-loginUser.userPassword = 'guest';
-setUserCookie();
+function guestLogin() {
+  loginUser.userName = "guest";
+  loginUser.userPassword = "guest";
+  setUserCookie();
   checkLogin();
 }
 
 function userLogin(e) {
   e.preventDefault();
 
-  let loginData = document.getElementById('input-login');
+  let loginData = document.getElementById("input-login");
   formData = new FormData(loginData);
 
   dataFromLogin(formData);
   setUserCookie();
   checkLogin();
-
 }
 
 function dataFromLogin(formData) {
-  loginUser.userName = formData.get('userName');
-  loginUser.userPassword = formData.get('userPassword');
-
-
+  loginUser.userName = formData.get("userName");
+  loginUser.userPassword = formData.get("userPassword");
 }
 function setUserCookie() {
-  document.cookie = "user=" + loginUser.userName +  "; path=/";
-
+  document.cookie = "user=" + loginUser.userName + "; path=/";
 }
 
 //
 
-
-
 function checkLogin() {
-
   if (checkUserAndPassword() === true && checkPassword() === true) {
-    window.open('/board.html', '_self');
-  }
-  else {
-    alert('wrong Username or Password')
+    window.open("/board.html", "_self");
+  } else {
+    alert("wrong Username or Password");
   }
 }
 
-function checkPassword(){ 
+function checkPassword() {
   for (let i = 0; i < userObjects.length; i++) {
-  if (userObjects[i]['userName'] == loginUser.userName && userObjects[i]['userPassword'] == loginUser.userPassword) {
-    loginUser.userPassword = "";
-    loginUser.userId = i;
-    return true;
+    if (
+      userObjects[i]["userName"] == loginUser.userName &&
+      userObjects[i]["userPassword"] == loginUser.userPassword
+    ) {
+      loginUser.userPassword = "";
+      loginUser.userId = i;
+      return true;
+    }
   }
-
+  loginUser.userPassword = "";
+  return false;
 }
-loginUser.userPassword = "";
-return false;
-
-
-}
-  
-
 
 /**
  * Data interface between server and Login.html
- * @param {*} e 
+ * @param {*} e
  */
 
 async function createNewUser(e) {
@@ -84,31 +74,25 @@ async function createNewUser(e) {
   let newUser = new User(GLOBAL_VARIABLES.currUserId, selectedAvatar);
   newUser.dataFromInput(formData);
 
-  if (checkForDouble(userObjects, 'userName', newUser.userName) === true || checkForDouble(userObjects, 'userEmail', newUser.userEmail) === true){
-    alert('Der Username oder die Emailadresse wird bereits verwendet');
-
-  }else{
+  if (
+    checkForDouble(userObjects, "userName", newUser.userName) === true ||
+    checkForDouble(userObjects, "userEmail", newUser.userEmail) === true
+  ) {
+    alert("Der Username oder die Emailadresse wird bereits verwendet");
+  } else {
     userObjects.push(newUser);
     GLOBAL_VARIABLES.currUserId++;
-    dataFromLogin(formData)
+    dataFromLogin(formData);
     // update data on server
     await saveToServer("userObjects", userObjects);
     await saveToServer("globalVariables", GLOBAL_VARIABLES);
-<<<<<<< HEAD
-
-    alert('New User Profile has been created!');
-    window.open('/index.html', '_self');
-=======
     setUserCookie();
-alert('the User '+ loginUser.userName +' was created successfully')
-//window.open('/index.html', '_self');
-checkLogin();
->>>>>>> 744e9c6ef61359328300889acaf89ab3298ad61d
+    alert("the User " + loginUser.userName + " was created successfully");
+    window.open('/index.html', '_self');
+    //checkLogin();
   }
 
-  
   // add new task to existing tasks
-  
 }
 
 function checkForDouble(array, reference, searchcriteria) {
@@ -119,19 +103,16 @@ function checkForDouble(array, reference, searchcriteria) {
   }
 }
 
-function logout(){
+function logout() {
   document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
+function selectAvatar(index) {
+  let avatar = ["avatar1.jpg", "avatar2.png", "avatar3.png", "avatar4.png"];
 
-function selectAvatar(index){
-  let avatar = ['avatar1.jpg', 'avatar2.png', 'avatar3.png', 'avatar4.png'];
+  let selected = document.getElementById("user-avatar");
 
-  let selected = document.getElementById('user-avatar');
+  selected.src = "img/" + avatar[index];
 
-  selected.src = 'img/' + avatar[index];
-
-  selectedAvatar = 'img/' + avatar[index];
-
+  selectedAvatar = "img/" + avatar[index];
 }
-
