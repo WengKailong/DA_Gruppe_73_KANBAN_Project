98e0f5d9de05dce61289fc825c2202e0e337f00c
kailong <wengkailong@gmail.com>
2021-06-09 12:11:17 +0200

    Delete Asigned Users added, Asigned Users scrollable added

--- addTask.js
+++ addTask.js
@@ -79,12 +79,18 @@ function loadAsignedUsers() {
      userId = asignedUsers[i];
     let user = new User();
     user = user.getUserById(userId, userObjects);
-    loadElement.innerHTML += `<div class=" d-flex flex-column align-items-center" style="width: 70px;">
-    <div class="form-profile-container">
-      <img src="${user.userProfileAvatar}" alt="" class="h-100" />
-    </div>
-    <span>${user.userName}</span>
-  </div>`;
+    loadElement.innerHTML += `
+                          <div class=" d-flex flex-column align-items-center position-relative" style="min-width: 70px;">
+                            <svg onclick="deleteAsignTo(${userId})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle profile-delete-icon" viewBox="0 0 16 16">
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                            <div class="form-profile-container">
+                              <img src="${user.userProfileAvatar}" alt="" class="h-100" />
+                            </div>
+                            <span>${user.userName}</span>
+                          </div>
+  `;
   }
 }
 
@@ -98,6 +104,14 @@ function asignTaskTo(userId) {
   }
 
   console.log(asignedUsers);
+  loadAsignedUsers();
+}
+
+function deleteAsignTo(userId){
+  
+  let idx = asignedUsers.indexOf(userId);
+  asignedUsers.splice(idx,1);
+  console.log('delete user from asigned to', userId, 'at index of', idx);
   loadAsignedUsers();
 }
 
